29c48e84d123d09dcaa77f6549942ab7b54068da
Pc-Emi <emiliodelpin@gmail.com>
2025-05-10 19:24:03 -0300

    Esqueleto Proyecto Security

---
+++ src/main/java/com/seguridad/ProSec/ProSecApplication.java
@@ -0,0 +1,13 @@
+package com.seguridad.ProSec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProSecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProSecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seguridad/ProSec/ProSecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seguridad.ProSec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
